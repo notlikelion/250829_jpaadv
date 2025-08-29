e7bf1b52c89e14e2db2f2217ab4d8428dc63ed80
notlikelion <notlikelion@gmail.com>
2025-08-29 15:06:45 +0900

    감사 기능 추가

--- src/main/java/com/example/jpaadv/model/entity/Post.java
+++ src/main/java/com/example/jpaadv/model/entity/Post.java
@@ -6,7 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
-public class Post {
+public class Post extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // MySQL
     private Long id;
