c2fd6f6874f63c2f95fa1afcbd67133ea6e22eec
notlikelion <notlikelion@gmail.com>
2025-08-29 16:30:36 +0900

    푸시

--- src/main/java/com/example/jpaadv/controller/PostController.java
+++ src/main/java/com/example/jpaadv/controller/PostController.java
@@ -1,11 +1,11 @@
 package com.example.jpaadv.controller;
 
+import com.example.jpaadv.model.dto.PostDTO;
 import com.example.jpaadv.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor // 의존성주입
@@ -17,5 +17,20 @@ public class PostController {
     public String list(Model model) {
         model.addAttribute("posts", postService.findAll());
         return "post/list"; // resources/templates/post/list.html
+    }
+
+    @GetMapping("/new")
+    public String createForm(Model model) {
+        // new PostDTO.SaveRequest() -> 빈 생성자
+        model.addAttribute("post", new PostDTO.SaveRequest());
+        return "post/form"; // resources/templates/post/form.html
+    }
+
+    @PostMapping("/new")
+    public String create(@ModelAttribute("post") PostDTO.SaveRequest dto) {
+        // new PostDTO.SaveRequest() -> 빈 생성자
+        postService.save(dto);
+        // P-R-G
+        return "redirect:/posts";
     }
 }
